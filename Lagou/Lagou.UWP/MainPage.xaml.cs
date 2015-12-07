fcc022b892559eec12c3c47eb6a5653ce271b82f
xlingfairy <gruan@asnum.com>
2015-12-08 00:04:23 +0800

    UWP Theme

--- Lagou/Lagou.UWP/MainPage.xaml.cs
+++ Lagou/Lagou.UWP/MainPage.xaml.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,42 @@ namespace Lagou.UWP {
             this.InitializeComponent();
 
             this.LoadApplication(new Lagou.App(IoC.Get<WinRTContainer>()));
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar")) {
+                StatusBar statusBar = StatusBar.GetForCurrentView();
+                statusBar.BackgroundColor = Color.FromArgb(0xff, 0x00, 0x97, 0xa7);//#0097A7
+                statusBar.BackgroundOpacity = 1;
+            }
+
+            this.Loaded += MainPage_Loaded;
+            DisplayInformation.GetForCurrentView().OrientationChanged += MainPage_OrientationChanged;
+        }
+
+        private async void MainPage_OrientationChanged(DisplayInformation sender, object args) {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar")) {
+                var flag = sender.CurrentOrientation != DisplayOrientations.Portrait;
+                StatusBar statusBar = StatusBar.GetForCurrentView();
+                if (flag)
+                    await statusBar.HideAsync();
+                else
+                    await statusBar.ShowAsync();
+            }
+        }
+
+        private void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            var btn = this.FindChildControl<ToggleButton>("splitViewToggle");
+            btn.Click += Btn_Click;
+
+            var backBtn = this.FindChildControl<ToggleButton>("backToggle");
+            backBtn.Click += BackBtn_Click;
+        }
+
+        private async void BackBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            await Lagou.App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private void Btn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            var spv = this.FindChildControl<SplitView>("masterDetailSplitView");
+            spv.IsPaneOpen = !spv.IsPaneOpen;
         }
     }
 }
---
+++ Lagou/Lagou.UWP/Renders/MasterDetailPageRender.cs
@@ -0,0 +1,33 @@
+﻿using Lagou.UWP.Renders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(MasterDetailPage), typeof(MasterDetailPageRender))]
+namespace Lagou.UWP.Renders {
+    public class MasterDetailPageRender : MasterDetailPageRenderer {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MasterDetailPage> e) {
+            base.OnElementChanged(e);
+
+            //this.Control.MasterTitleVisibility = Windows.UI.Xaml.Visibility.Visible;
+            //this.Control.ToolbarBackground = new SolidColorBrush(Colors.Purple);
+
+            this.Control.Master.SizeChanged += Master_SizeChanged;
+        }
+
+        private void Master_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e) {
+            // if not do this , master page content not show
+            this.Element.Master.Layout(new Rectangle(0, 0, e.NewSize.Width, e.NewSize.Height));
+        }
+    }
+}
