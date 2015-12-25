96a985cf88604c3f618dc7b0c678137218e89d95
gruan01 <gruan@asnum.com>
2015-12-25 18:11:55 +0800

    111

--- Lagou.API/AngleSharpHelper.cs
+++ Lagou.API/AngleSharpHelper.cs
@@ -23,44 +23,46 @@ namespace Lagou.API {
         }
 
         public static T Parse<T>(this HtmlParser parser, string source) where T : class, new() {
-            var doc = parser.Parse(source);
 
-            var t = new T();
-            var ps = typeof(T).GetRuntimeProperties();
-            #region
-            foreach (var p in ps) {
-                var attr = p.GetCustomAttribute<HtmlQueryAttribute>();
-                if (attr != null) {
-                    var ele = doc.QuerySelector(attr.Selector);
-                    if (ele != null) {
-                        string value = "";
-                        switch (attr.ValueTarget) {
-                            case HtmlQueryValueTargets.Attribute:
-                                var a = ele.Attributes.GetNamedItem(attr.AttributeName);
-                                if (a != null)
-                                    value = a.Value.Trim();
-                                break;
-                            case HtmlQueryValueTargets.InnerHtml:
-                                value = ele.InnerHtml.Trim();
-                                break;
-                            case HtmlQueryValueTargets.Text:
-                                value = ele.TextContent.Trim();
-                                break;
-                        }
+            using (var doc = parser.Parse(source)) {
 
-                        try {
-                            if (!p.PropertyType.Equals(typeof(string))) {
-                                var v = Convert.ChangeType(value, p.PropertyType);
-                                p.SetValue(t, v);
-                            } else {
-                                p.SetValue(t, value);
+                var t = new T();
+                var ps = typeof(T).GetRuntimeProperties();
+                #region
+                foreach (var p in ps) {
+                    var attr = p.GetCustomAttribute<HtmlQueryAttribute>();
+                    if (attr != null) {
+                        var ele = doc.QuerySelector(attr.Selector);
+                        if (ele != null) {
+                            string value = "";
+                            switch (attr.ValueTarget) {
+                                case HtmlQueryValueTargets.Attribute:
+                                    var a = ele.Attributes.GetNamedItem(attr.AttributeName);
+                                    if (a != null)
+                                        value = a.Value.Trim();
+                                    break;
+                                case HtmlQueryValueTargets.InnerHtml:
+                                    value = ele.InnerHtml.Trim();
+                                    break;
+                                case HtmlQueryValueTargets.Text:
+                                    value = ele.TextContent.Trim();
+                                    break;
                             }
-                        } catch { }
+
+                            try {
+                                if (!p.PropertyType.Equals(typeof(string))) {
+                                    var v = Convert.ChangeType(value, p.PropertyType);
+                                    p.SetValue(t, v);
+                                } else {
+                                    p.SetValue(t, value);
+                                }
+                            } catch { }
+                        }
                     }
                 }
+                #endregion
+                return t;
             }
-            #endregion
-            return t;
         }
     }
 }
--- Lagou/Lagou/ViewModels/IndexViewModel.cs
+++ Lagou/Lagou/ViewModels/IndexViewModel.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Lagou.ViewModels {
     public class IndexViewModel : BaseVM {
@@ -23,21 +25,65 @@ namespace Lagou.ViewModels {
 
         public BindableCollection<SearchedItemViewModel> Datas { get; set; }
 
+        public bool InRefresh { get; set; }
+
+        public ICommand ReloadCmd { get; set; }
+
+        public ICommand LoadMoreCmd { get; set; }
+
+        private int Page = 1;
+
         public IndexViewModel(SimpleContainer container, INavigationService ns) {
             this.Datas = new BindableCollection<SearchedItemViewModel>();
             this.Container = container;
             this.NS = ns;
+
+            this.ReloadCmd = new Command(async () => {
+                await this.LoadData(true);
+            });
+
+            this.LoadMoreCmd = new Command(async () => {
+                await this.LoadData(false);
+            });
         }
 
         protected async override void OnActivate() {
             base.OnActivate();
+            await this.LoadData();
+        }
 
-            var method = new Search();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reload">是否是重新加载</param>
+        /// <returns></returns>
+        private async Task LoadData(bool reload = false) {
+            this.InRefresh = true;
+            Device.BeginInvokeOnMainThread(() => {
+                this.NotifyOfPropertyChange(() => this.InRefresh);
+            });
+
+            var method = new Search() {
+                Page = reload ? 1 : this.Page
+            };
             var datas = await ApiClient.Execute(method);
-            this.Datas.AddRange(datas.Select(d =>
-                new SearchedItemViewModel(d, this.NS)
-            ));
-            this.NotifyOfPropertyChange(() => this.Datas);
+            if (!method.HasError && datas.Count() > 0) {
+
+                if (reload) {
+                    this.Datas.Clear();
+                }
+
+                this.Datas.AddRange(datas.Select(d =>
+                    new SearchedItemViewModel(d, this.NS)
+                ));
+                this.NotifyOfPropertyChange(() => this.Datas);
+                this.Page++;
+            }
+
+            this.InRefresh = false;
+            Device.BeginInvokeOnMainThread(() => {
+                this.NotifyOfPropertyChange(() => this.InRefresh);
+            });
         }
     }
 }
