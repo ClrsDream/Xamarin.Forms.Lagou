6164da59d17f39ddf41cadc35694d12590aa7acb
gruan01 <gruan@asnum.com>
2015-12-30 18:45:55 +0800

    111

---
+++ Lagou/Lagou.UWP/Class1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagou.UWP {
+
+    internal class NotifyCollectionChangedEventArgsEx : NotifyCollectionChangedEventArgs {
+        public int Count { get; private set; }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action)
+          : base(action) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, IList changedItems)
+          : base(action, changedItems) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, IList newItems, IList oldItems)
+          : base(action, newItems, oldItems) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, IList newItems, IList oldItems, int startingIndex)
+          : base(action, newItems, oldItems, startingIndex) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, IList changedItems, int startingIndex)
+          : base(action, changedItems, startingIndex) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, IList changedItems, int index, int oldIndex)
+          : base(action, changedItems, index, oldIndex) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, object changedItem)
+          : base(action, changedItem) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, object changedItem, int index)
+          : base(action, changedItem, index) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, object changedItem, int index, int oldIndex)
+          : base(action, changedItem, index, oldIndex) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, object newItem, object oldItem)
+          : base(action, newItem, oldItem) {
+            this.Count = count;
+        }
+
+        public NotifyCollectionChangedEventArgsEx(int count, NotifyCollectionChangedAction action, object newItem, object oldItem, int index)
+          : base(action, newItem, oldItem, index) {
+            this.Count = count;
+        }
+    }
+
+
+
+
+
+
+    internal static class NotifyCollectionChangedEventArgsExtensions {
+        public static void Apply<TFrom>(this NotifyCollectionChangedEventArgs self, IList<TFrom> from, IList<object> to) {
+            int num = (int)NotifyCollectionChangedEventArgsExtensions.Apply(self, (Action<object, int, bool>)((o, i, b) => to.Insert(i, o)), (Action<object, int>)((o, i) => to.RemoveAt(i)), (Action)(() =>
+            {
+                to.Clear();
+                for (int index = 0; index < ((ICollection<TFrom>)from).Count; ++index)
+                    to.Add((object)from[index]);
+            }));
+        }
+
+        public static NotifyCollectionChangedAction Apply(this NotifyCollectionChangedEventArgs self, Action<object, int, bool> insert, Action<object, int> removeAt, Action reset) {
+            if (self == null)
+                throw new ArgumentNullException("self");
+            if (reset == null)
+                throw new ArgumentNullException("reset");
+            if (insert == null)
+                throw new ArgumentNullException("insert");
+            if (removeAt == null)
+                throw new ArgumentNullException("removeAt");
+            switch (self.Action) {
+                case NotifyCollectionChangedAction.Add:
+                    if (self.NewStartingIndex >= 0) {
+                        for (int index = 0; index < self.NewItems.Count; ++index)
+                            insert(self.NewItems[index], index + self.NewStartingIndex, true);
+                        break;
+                    }
+                    goto case 4;
+                case NotifyCollectionChangedAction.Remove:
+                    if (self.OldStartingIndex >= 0) {
+                        for (int index = 0; index < self.OldItems.Count; ++index)
+                            removeAt(self.OldItems[index], self.OldStartingIndex);
+                        break;
+                    }
+                    goto case 4;
+                case NotifyCollectionChangedAction.Replace:
+                    if (self.OldStartingIndex >= 0) {
+                        for (int index = 0; index < self.OldItems.Count; ++index) {
+                            removeAt(self.OldItems[index], index + self.OldStartingIndex);
+                            insert(self.OldItems[index], index + self.OldStartingIndex, true);
+                        }
+                        break;
+                    }
+                    goto case 4;
+                case NotifyCollectionChangedAction.Move:
+                    if (self.NewStartingIndex >= 0 && self.OldStartingIndex >= 0) {
+                        for (int index = 0; index < self.OldItems.Count; ++index)
+                            removeAt(self.OldItems[index], self.OldStartingIndex);
+                        int newStartingIndex = self.NewStartingIndex;
+                        if (self.OldStartingIndex < self.NewStartingIndex)
+                            newStartingIndex -= self.OldItems.Count - 1;
+                        for (int index = 0; index < self.OldItems.Count; ++index)
+                            insert(self.OldItems[index], newStartingIndex + index, false);
+                        break;
+                    }
+                    goto case 4;
+                case NotifyCollectionChangedAction.Reset:
+                    reset();
+                    return NotifyCollectionChangedAction.Reset;
+            }
+            return self.Action;
+        }
+
+        public static NotifyCollectionChangedEventArgsEx WithCount(this NotifyCollectionChangedEventArgs e, int count) {
+            switch (e.Action) {
+                case NotifyCollectionChangedAction.Add:
+                    return new NotifyCollectionChangedEventArgsEx(count, NotifyCollectionChangedAction.Add, e.NewItems, e.NewStartingIndex);
+                case NotifyCollectionChangedAction.Remove:
+                    return new NotifyCollectionChangedEventArgsEx(count, NotifyCollectionChangedAction.Remove, e.OldItems, e.OldStartingIndex);
+                case NotifyCollectionChangedAction.Replace:
+                    return new NotifyCollectionChangedEventArgsEx(count, NotifyCollectionChangedAction.Replace, e.NewItems, e.OldItems, e.OldStartingIndex);
+                case NotifyCollectionChangedAction.Move:
+                    return new NotifyCollectionChangedEventArgsEx(count, NotifyCollectionChangedAction.Move, e.OldItems, e.NewStartingIndex, e.OldStartingIndex);
+                default:
+                    return new NotifyCollectionChangedEventArgsEx(count, NotifyCollectionChangedAction.Reset);
+            }
+        }
+    }
+}
--- Lagou/Lagou.UWP/Renders/TabbedPageRender.cs
+++ Lagou/Lagou.UWP/Renders/TabbedPageRender.cs
@@ -9,19 +9,102 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Reflection;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
-//[assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedPageRender))]
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedPageRender))]
 namespace Lagou.UWP.Renders {
-    public class TabbedPageRender : TabbedPageRenderer {
+    //public class TabbedPageRender : TabbedPageRenderer {
 
-        public TabbedPageRender() {
-            this.ElementChanged += TabbedPageRender_ElementChanged;
+    //    public TabbedPageRender() {
+    //        this.ElementChanged += TabbedPageRender_ElementChanged;
+    //    }
+
+    //    private void TabbedPageRender_ElementChanged(object sender, VisualElementChangedEventArgs e) {
+    //        if(this.Control != null) {
+    //            this.Control.Style = (Windows.UI.Xaml.Style)Windows.UI.Xaml.Application.Current.Resources["TabbedPageStyle2"];
+    //        }
+    //    }
+    //}
+
+    public class TabbedPageRender : VisualElementRenderer<TabbedPage, Pivot> {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e) {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null) {
+                var p = new Pivot();
+                this.SetNativeControl(p);
+                p.Loaded += P_Loaded;
+                p.Unloaded += P_Unloaded;
+                p.SelectionChanged += P_SelectionChanged;
+                p.DataContext = this.Element;
+                this.OnPagesChanged(this.Element.Children, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+                ((INotifyCollectionChanged)this.Element.Children).CollectionChanged += this.OnPagesChanged;
+            }
         }
 
-        private void TabbedPageRender_ElementChanged(object sender, VisualElementChangedEventArgs e) {
-            if(this.Control != null) {
-                this.Control.Style = (Windows.UI.Xaml.Style)Windows.UI.Xaml.Application.Current.Resources["TabbedPageStyle2"];
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e) {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName.Equals("CurrentPage")) {
+                this.UpdateCurrentPage();
+            } else if (e.PropertyName.Equals(Xamarin.Forms.Page.TitleProperty.PropertyName)) {
+                //this.UpdateTitle();
             }
+        }
+
+        public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint) {
+            var availableSize = new Windows.Foundation.Size(widthConstraint, heightConstraint);
+            double width = this.Control.Width;
+            double height = this.Control.Height;
+            this.Control.Height = double.NaN;
+            this.Control.Width = double.NaN;
+            this.Control.Measure(availableSize);
+            var arg_A2_0 = new Size(Math.Ceiling(this.Control.DesiredSize.Width), Math.Ceiling(this.Control.DesiredSize.Height));
+            this.Control.Width = width;
+            this.Control.Height = height;
+            return new SizeRequest(arg_A2_0);
+        }
+
+        private void UpdateCurrentPage() {
+            var currentPage = this.Element.CurrentPage;
+            if (currentPage == null) {
+                return;
+            }
+            this.Control.SelectedItem = currentPage;
+        }
+
+        private void P_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (this.Element != null) {
+                var currPage = (Xamarin.Forms.Page)e.AddedItems?[0];
+                this.Element.CurrentPage = currPage;
+            }
+        }
+
+        private void P_Unloaded(object sender, RoutedEventArgs e) {
+            if (this.Element != null) {
+                var mhd = this.Element.GetType().GetMethod("SendDisappearing", BindingFlags.NonPublic | BindingFlags.Instance);
+                mhd.Invoke(this.Element, new object[] { });
+            }
+        }
+
+        private void P_Loaded(object sender, RoutedEventArgs e) {
+            if (this.Element != null) {
+                var mhd = this.Element.GetType().GetMethod("SendAppearing", BindingFlags.NonPublic | BindingFlags.Instance);
+                mhd.Invoke(this.Element, new object[] { });
+            }
+        }
+
+        private void OnPagesChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            if (this.Element == null || this.Control == null)
+                return;
+
+            e.Apply(this.Element.Children, this.Control.Items);
+            this.Control.UpdateLayout();
         }
     }
 }
