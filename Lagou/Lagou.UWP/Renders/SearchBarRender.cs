baae9f67031ced5b9dc5db460de77aa411cc2a33
gruan01 <gruan@asnum.com>
2016-01-11 11:47:49 +0800

    Hide Keyboard when searchbar commited.

---
+++ Lagou/Lagou.UWP/Renders/SearchBarRender.cs
@@ -0,0 +1,29 @@
+﻿using Lagou.UWP.Renders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(SearchBarRender))]
+namespace Lagou.UWP.Renders {
+    public class SearchBarRender : SearchBarRenderer {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e) {
+            base.OnElementChanged(e);
+
+            if(this.Control != null) {
+                this.Control.QuerySubmitted += Control_QuerySubmitted;
+            }
+
+        }
+
+        private void Control_QuerySubmitted(Windows.UI.Xaml.Controls.AutoSuggestBox sender, Windows.UI.Xaml.Controls.AutoSuggestBoxQuerySubmittedEventArgs args) {
+            InputPane.GetForCurrentView().TryHide();
+        }
+    }
+}
