2c2b8bb47ccfdeddbc035873298df2838ef5b50c
gruan01 <gruan@asnum.com>
2016-01-15 14:25:27 +0800

    111

--- Lagou/Lagou.Droid/Resources/Resource.Designer.cs
+++ Lagou/Lagou.Droid/Resources/Resource.Designer.cs
@@ -3339,11 +3339,14 @@ namespace Lagou.Droid
 			// aapt resource value: 0x7f020062
 			public const int mr_ic_settings_light = 2130837602;
 			
-			// aapt resource value: 0x7f020064
-			public const int notification_template_icon_bg = 2130837604;
+			// aapt resource value: 0x7f020065
+			public const int notification_template_icon_bg = 2130837605;
 			
 			// aapt resource value: 0x7f020063
-			public const int SplashScreen = 2130837603;
+			public const int Splash = 2130837603;
+			
+			// aapt resource value: 0x7f020064
+			public const int SplashScreen = 2130837604;
 			
 			static Drawable()
 			{
